5cd25aa43f404a58f41cae26e1fbb05eab8fbd95
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2025-06-12 21:55:41 -0600

    Content Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import { useState } from 'react'
 import './App.css'
 import ArticlePreview from './components/ArticlePreview'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
--- src/components/ArticlePreview.jsx
+++ src/components/ArticlePreview.jsx
@@ -1,10 +1,32 @@
 import React from "react";
 import "./ArticlePreview.css";
+import articleImage from "../assets/drawers.jpg";
 
 function ArticlePreview() {
     return (
-        <div>
-            <h1>Article Preview</h1>
+        <div className="article-preview-wrapper">
+            <div className="article-image">
+                <img src={articleImage} alt="Article Image" />
+            </div>
+            <div className="article-content">
+            <h2 className="article-title">Shift the overall look and feel by adding these wonderful 
+  touches to furniture in your home</h2>
+
+  <p className="article-content-text">Ever been in a room and felt like something was missing? Perhaps 
+  it felt slightly bare and uninviting. I’ve got some simple tips 
+  to help you make any room feel complete.</p>
+
+            </div>
+            <div className="article-footer">
+                <div className="article-author">
+                    <img src="" alt="" />
+                    <div className="article-author-info">
+                        <h2>Michelle Appleton</h2>
+                        <p>28 Jun 2020</p>
+                    </div>
+                </div>
+                <button>Share</button>
+            </div>
         </div>
     );
 }
